5ca603a44cc472097aba96c2c72c0b908805bde0
YOSRA NABI <yosra.nabi97@gmail.com>
2021-10-21 14:15:26 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 
 import './App.css';
-import {BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import FilmOrdonee from './components/FilmOrdonee';
 import SearchBar from './components/SearchBar';
 import Filmdetail from './components/Filmdetail';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Route exact path="/movie/populair" component={FilmOrdonee} />
-      <Route exact path="/search/multi" component={SearchBar} />
-      <Route exact path ="/Movie/:ID" component={Filmdetail}/>
-     
+        <Route exact path="/movie/populair" component={FilmOrdonee} />
+        <Route exact path="/search/multi" component={SearchBar} />
+        <Route exact path="/Movie/:ID" component={Filmdetail} />
       </BrowserRouter>
     </div>
   );
--- src/components/FilmOrdonee.js
+++ src/components/FilmOrdonee.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import '../App.css'
-import { ListGroup,Navbar,Nav,Button} from 'react-bootstrap'
+import { ListGroup, Navbar, Nav, Button } from 'react-bootstrap'
 
 const FilmOrdonee = () => {
   const Location = useLocation()
@@ -17,44 +17,43 @@ const FilmOrdonee = () => {
         setData(res.results);
       });
   });
-    return (
-      <div >
-        <Navbar bg="light" variant="dark">
-          <h7 className="Couleur">
-            SiteFilm
-          </h7>
-          <br />
-          <Nav.Link href="/search/multi">Recherche</Nav.Link>
-          </Navbar>
+  return (
+    <div >
+      <Navbar bg="light" variant="dark">
+        <h7 className="Couleur">
+          SiteFilm
+        </h7>
         <br />
-        <ListGroup>
-          {data.map(film => {
-            return (
-              <div>
-                {console.log(film)}
-                <ListGroup.Item>
-                  <ListGroup horizontal>
-                    <ListGroup.Item className="Taille">
-                      {film.title}
-                    </ListGroup.Item>
-                    <ListGroup.Item >
-                      <img width="400" src={"https://image.tmdb.org/t/p/w500/" + film.backdrop_path} alt={"Image de film " + film.title} ></img>
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                      <Link to={"/Movie/" + film.id}>
-                        <Button variant="outline-success">Voir détail</Button>
-                      </Link>
-                    </ListGroup.Item>
-                  </ListGroup>
-                </ListGroup.Item>
-  
-              </div>
-            )
-          }
-          )}
-        </ListGroup>
-      </div >
-    );
+        <Nav.Link href="/search/multi">Recherche</Nav.Link>
+      </Navbar>
+      <br />
+      <ListGroup>
+        {data.map(film => {
+          return (
+            <div>
+              {console.log(film)}
+              <ListGroup.Item>
+                <ListGroup horizontal>
+                  <ListGroup.Item className="Taille">
+                    {film.title}
+                  </ListGroup.Item>
+                  <ListGroup.Item >
+                    <img width="400" src={"https://image.tmdb.org/t/p/w500/" + film.backdrop_path} alt={"Image de film " + film.title} ></img>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Link to={"/Movie/" + film.id}>
+                      <Button variant="outline-success">Voir détail</Button>
+                    </Link>
+                  </ListGroup.Item>
+                </ListGroup>
+              </ListGroup.Item>
+            </div>
+          )
+        }
+        )}
+      </ListGroup>
+    </div >
+  );
 };
 
 export default FilmOrdonee;
